0d160d120c263e72c948ec3202bd9e97563ead9d
Dongfeng <gudongfeng@outlook.com>
2016-11-03 15:00:35 -0400

    modify the convert.sh script

--- data_preparation/convert.sh
+++ data_preparation/convert.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-for folder in /path/to/video/folder/*
+# convert the avi video to images
+#   Usage (sudo for the remove priviledge):
+#       sudo ./conver.sh path/to/video/
+#   Example Usage:
+#       sudo ./conver.sh ~/document/videofile/
+#   Example Output:
+#       ~/document/videofile/walk/video1.avi 
+#       #=>
+#       ~/document/videofile/walk/video1/00001.jpg
+#       ~/document/videofile/walk/video1/00002.jpg
+#       ~/document/videofile/walk/video1/00003.jpg
+#       ~/document/videofile/walk/video1/00004.jpg
+#       ~/document/videofile/walk/video1/00005.jpg
+#       ...
+
+for folder in $1*
 do
-    count = 0
     for file in "$folder"/*.avi
     do
-        if [[ ! -d "$folder"/$count ]]; then
-            mkdir -p "$folder"/$count
+        echo "${file[@]%.avi}"
+        if [[ ! -d "${file[@]%.avi}" ]]; then
+            mkdir -p "${file[@]%.avi}"
         fi
-        ffmpeg -i "$file" -vf fps=20 "$folder"/$count/%05d.png
+        ffmpeg -i "$file" -vf fps=5 "${file[@]%.avi}"/%05d.jpg
         rm "$file"
-        (( count++ ))
     done
 done
