92e5ab758c45d5d683781210f2391d9213802b21
Dongfeng <gudongfeng@outlook.com>
2016-11-15 17:17:58 -0500

    change the file indent size

--- cnn_model.py
+++ cnn_model.py
@@ -20,9 +20,9 @@ from six.moves import urllib
 import tensorflow as tf
 
 tf.app.flags.DEFINE_string('model_dir', '/tmp/imagenet',
-                            """Path to classify_image_graph_def.pb,"""
-                            """imagenet_synset_to_human_label_map.txt, and"""
-                            """imagenet_2012_challenge_label_map_proto.pbtxt.""")
+              """Path to classify_image_graph_def.pb,"""
+              """imagenet_synset_to_human_label_map.txt, and"""
+              """imagenet_2012_challenge_label_map_proto.pbtxt.""")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -31,28 +31,28 @@ DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-
 # pylint: enable=line-too-long
 
 def create_graph():
-    """Creates a graph from saved GraphDef file and returns a saver."""
-    # Creates graph from saved graph_def.pb.
-    with tf.gfile.FastGFile(os.path.join(
-            FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        _ = tf.import_graph_def(graph_def, name='')
+  """Creates a graph from saved GraphDef file and returns a saver."""
+  # Creates graph from saved graph_def.pb.
+  with tf.gfile.FastGFile(os.path.join(
+      FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
 
 def maybe_download_and_extract():
-    """Download and extract model tar file."""
-    dest_directory = FLAGS.model_dir
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
-    filename = DATA_URL.split('/')[-1]
-    filepath = os.path.join(dest_directory, filename)
-    if not os.path.exists(filepath):
-        def _progress(count, block_size, total_size):
-            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
-                filename, float(count * block_size) / float(total_size) * 100.0))
-            sys.stdout.flush()
-        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
-        print()
-        statinfo = os.stat(filepath)
-        print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
-    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+  """Download and extract model tar file."""
+  dest_directory = FLAGS.model_dir
+  if not os.path.exists(dest_directory):
+    os.makedirs(dest_directory)
+  filename = DATA_URL.split('/')[-1]
+  filepath = os.path.join(dest_directory, filename)
+  if not os.path.exists(filepath):
+    def _progress(count, block_size, total_size):
+      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+        filename, float(count * block_size) / float(total_size) * 100.0))
+      sys.stdout.flush()
+    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+    print()
+    statinfo = os.stat(filepath)
+    print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
+  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
--- video_processing.py
+++ video_processing.py
@@ -1,18 +1,18 @@
 """ Read and preprocess video data.
-    Video processing occurs on a single video at a time. Video are read and
-    preprocessed in parallel across multiple threads. The resulting videos
-    are concatenated together to form a single batch for training or 
-    evaluation.
+  Video processing occurs on a single video at a time. Video are read and
+  preprocessed in parallel across multiple threads. The resulting videos
+  are concatenated together to form a single batch for training or 
+  evaluation.
 
-    -- Provide processed video data for a network:
-    inputs: Construct batches of evaluation examples of videos.
-    distorted_inputs: Construct batches of training examples of videos.
-    batch_inputs: Construct batches of training or evaluation examples of 
-        videos.
+  -- Provide processed video data for a network:
+  inputs: Construct batches of evaluation examples of videos.
+  distorted_inputs: Construct batches of training examples of videos.
+  batch_inputs: Construct batches of training or evaluation examples of 
+    videos.
 
-    -- Data processing:
-    parse_example_proto: Parses an Example proto containing a training example
-        of a video.
+  -- Data processing:
+  parse_example_proto: Parses an Example proto containing a training example
+    of a video.
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -24,236 +24,236 @@ import settings
 FLAGS = tf.app.flags.FLAGS
 
 def inputs(dataset, batch_size=None, num_preprocess_threads=None):
-    """ Generate batches of videos for evaluation.
+  """ Generate batches of videos for evaluation.
 
-    Use this function as the inputs for evaluating a network.
+  Use this function as the inputs for evaluating a network.
 
-    Note that some (minimal) video preprocessing occurs during evaluation
-    including central cropping and resizing of the video to fit the network.
+  Note that some (minimal) video preprocessing occurs during evaluation
+  including central cropping and resizing of the video to fit the network.
 
-    Args:
-        dataset: instance of Dataset class specifying the dataset.
-        batch_size: integer, number of examples in batch, default value is 10
-        num_preprocess_threads: integer, total number of preprocessing threads
-            defaults to FLAGS.num_preprocess_threads.
+  Args:
+    dataset: instance of Dataset class specifying the dataset.
+    batch_size: integer, number of examples in batch, default value is 10
+    num_preprocess_threads: integer, total number of preprocessing threads
+      defaults to FLAGS.num_preprocess_threads.
 
-    Returns:
-        videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
-            video, each video is a dictionary containing strings providing 
-            JPEG encoding of all the images of a video clip
-        labels: 1-D integer Tensor of [FLAGS.batch_size].
-        filenames: 1-D integer Tensor of [FLAGS.batch_size].
-    """
-    if not batch_size:
-        batch_size = FLAGS.batch_size
+  Returns:
+    videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
+      video, each video is a dictionary containing strings providing 
+      JPEG encoding of all the images of a video clip
+    labels: 1-D integer Tensor of [FLAGS.batch_size].
+    filenames: 1-D integer Tensor of [FLAGS.batch_size].
+  """
+  if not batch_size:
+    batch_size = FLAGS.batch_size
 
-    # Force all input processing onto CPU in order to reserve the GPU for
-    # the forward inference and back-propagation.
-    with tf.device('/cpu:0'):
-        videos, labels, filenames = batch_inputs(
-            dataset, batch_size, train=False,
-            num_preprocess_threads=num_preprocess_threads)
+  # Force all input processing onto CPU in order to reserve the GPU for
+  # the forward inference and back-propagation.
+  with tf.device('/cpu:0'):
+    videos, labels, filenames = batch_inputs(
+      dataset, batch_size, train=False,
+      num_preprocess_threads=num_preprocess_threads)
 
-    return videos, labels, filenames
+  return videos, labels, filenames
 
 
 def distorted_inputs(dataset, batch_size=None, num_preprocess_threads=None):
-    """ Generate batches of distorted versions of videos.
+  """ Generate batches of distorted versions of videos.
 
-    Use this function as the inputs for training a network.
+  Use this function as the inputs for training a network.
 
-    Distorting videos provides a useful technique for augmenting the data
-    set during training in order to make the network invariant to aspects
-    of the video that do not effect the label.
+  Distorting videos provides a useful technique for augmenting the data
+  set during training in order to make the network invariant to aspects
+  of the video that do not effect the label.
 
-    Args:
-        dataset: instance of Dataset class specifying the dataset.
-        batch_size: integer, number of examples in batch
-        num_preprocess_threads: integer, total number of preprocessing threads
-            defaults to FLAGS.num_preprocess_threads.
+  Args:
+    dataset: instance of Dataset class specifying the dataset.
+    batch_size: integer, number of examples in batch
+    num_preprocess_threads: integer, total number of preprocessing threads
+      defaults to FLAGS.num_preprocess_threads.
 
-    Returns:
-        videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
-            video, each video is a dictionary containing strings providing 
-            JPEG encoding of all the images of a video clip
-        labels: 1-D integer one host Tensor of [batch_size].
-        filenames: 1-D integer Tensor of [batch_size].
-    """
-    if not batch_size:
-        batch_size = FLAGS.batch_size
+  Returns:
+    videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
+      video, each video is a dictionary containing strings providing 
+      JPEG encoding of all the images of a video clip
+    labels: 1-D integer one host Tensor of [batch_size].
+    filenames: 1-D integer Tensor of [batch_size].
+  """
+  if not batch_size:
+    batch_size = FLAGS.batch_size
 
-    # Force all input processing onto CPU in order to reserve the GPU for
-    # the forward inference and back-propagation.
-    videos, labels_one_hot, filenames = batch_inputs(
-        dataset, batch_size, train=True,
-        num_preprocess_threads=num_preprocess_threads)
-    return videos, labels_one_hot, filenames
+  # Force all input processing onto CPU in order to reserve the GPU for
+  # the forward inference and back-propagation.
+  videos, labels_one_hot, filenames = batch_inputs(
+    dataset, batch_size, train=True,
+    num_preprocess_threads=num_preprocess_threads)
+  return videos, labels_one_hot, filenames
 
 def video_preprocessing(image_features):
-    """ Transfer dictionary to tensor type
+  """ Transfer dictionary to tensor type
 
-    Args:
-        image_features: dictionary contains, Tensor tf.string containing the 
-            contents of all the JPEG file of a video.
+  Args:
+    image_features: dictionary contains, Tensor tf.string containing the 
+      contents of all the JPEG file of a video.
 
-    Returns:
-        resutl: 4-D float Tensor containing an appropriately list of scaled image
-            [sequence_length, encoded JPEG string]
+  Returns:
+    resutl: 4-D float Tensor containing an appropriately list of scaled image
+      [sequence_length, encoded JPEG string]
 
-    Raises:
-        ValueError: if user does not provide bounding box
-    """
-    # convert the image_features dictionary to array
-    images = []
-    tmp_dict = {}
-    for key, value in image_features.items(): 
-        tmp_dict[int(key[-3:])] = image_features[key]
-    image_features.clear()
-    for index in range(len(tmp_dict)):
-        images.append(tmp_dict[index])
+  Raises:
+    ValueError: if user does not provide bounding box
+  """
+  # convert the image_features dictionary to array
+  images = []
+  tmp_dict = {}
+  for key, value in image_features.items(): 
+    tmp_dict[int(key[-3:])] = image_features[key]
+  image_features.clear()
+  for index in range(len(tmp_dict)):
+    images.append(tmp_dict[index])
 
-     # transfer the images list into a tensor
-    for idx, image in enumerate(images):
-        images[idx] = tf.expand_dims(image, 0)
-    result = tf.concat(0, images)
-    return result
+   # transfer the images list into a tensor
+  for idx, image in enumerate(images):
+    images[idx] = tf.expand_dims(image, 0)
+  result = tf.concat(0, images)
+  return result
 
 def parse_example_proto(example_serialized):
-    """ Parses an Example proto containing a training example of a video clip.
+  """ Parses an Example proto containing a training example of a video clip.
 
-    The output of the convert_to_records.py video preprocessing script is a 
-    dataset containing serialized Example protocol buffers. Each Example proto 
-    contains the following fields:
+  The output of the convert_to_records.py video preprocessing script is a 
+  dataset containing serialized Example protocol buffers. Each Example proto 
+  contains the following fields:
 
-        image/height: 200
-        image/width: 100
-        image/colorspace: 'RGB'
-        image/channels: 3
-        image/class/label: 2
-        image/class/text: 'walking'
-        image/format: 'JPEG'
-        image/filename: '00001.JPEG'
-        raw/image/001: <JPEG encoded string>
-        ...
-        raw/image/n: <JPEG encoded string>
+    image/height: 200
+    image/width: 100
+    image/colorspace: 'RGB'
+    image/channels: 3
+    image/class/label: 2
+    image/class/text: 'walking'
+    image/format: 'JPEG'
+    image/filename: '00001.JPEG'
+    raw/image/001: <JPEG encoded string>
+    ...
+    raw/image/n: <JPEG encoded string>
 
-    Args:
-        example_serialized: scalar Tensor tf.string containing a serialized
-        Example protocol buffer.
+  Args:
+    example_serialized: scalar Tensor tf.string containing a serialized
+    Example protocol buffer.
 
-    Returns:
-        image_features: A dictionary containing strings providing JPEG
-            encoding of all the images of a video clip.
-        label: Tensor tf.int32 containing the label.
-        text: Tensor tf.string containing the human-readable label.
-        filename: the filename of the image
-    """
-    # Dense features in Example proto.
-    feature_map = {
-        'image/class/label': tf.FixedLenFeature([1], dtype=tf.int64,
-                                                default_value=-1),
-        'image/class/text': tf.FixedLenFeature([], dtype=tf.string,
-                                                default_value=''),
-        'image/filename': tf.FixedLenFeature([], dtype=tf.string,
-                                            default_value='')
-    }
-    features = tf.parse_single_example(example_serialized, feature_map)
-    label = tf.cast(features['image/class/label'], dtype=tf.int32)
-    # subtract the label value by 1, becuae the previous label value range
-    #  from(1..n)
-    label = tf.sub(label, tf.constant(1))
+  Returns:
+    image_features: A dictionary containing strings providing JPEG
+      encoding of all the images of a video clip.
+    label: Tensor tf.int32 containing the label.
+    text: Tensor tf.string containing the human-readable label.
+    filename: the filename of the image
+  """
+  # Dense features in Example proto.
+  feature_map = {
+    'image/class/label': tf.FixedLenFeature([1], dtype=tf.int64,
+                        default_value=-1),
+    'image/class/text': tf.FixedLenFeature([], dtype=tf.string,
+                        default_value=''),
+    'image/filename': tf.FixedLenFeature([], dtype=tf.string,
+                      default_value='')
+  }
+  features = tf.parse_single_example(example_serialized, feature_map)
+  label = tf.cast(features['image/class/label'], dtype=tf.int32)
+  # subtract the label value by 1, becuae the previous label value range
+  #  from(1..n)
+  label = tf.sub(label, tf.constant(1))
 
-    # images data in the Example proto
-    image_map = {}
-    for index in range(FLAGS.sequence_size):
-        image_map['raw/image/%03d' % index] = tf.FixedLenFeature(
-        [], 
-        dtype=tf.string,
-        default_value='')
-    image_features = tf.parse_single_example(example_serialized, image_map) 
+  # images data in the Example proto
+  image_map = {}
+  for index in range(FLAGS.sequence_size):
+    image_map['raw/image/%03d' % index] = tf.FixedLenFeature(
+    [], 
+    dtype=tf.string,
+    default_value='')
+  image_features = tf.parse_single_example(example_serialized, image_map) 
 
-    return (image_features,
-            label,
-            features['image/class/text'],
-            features['image/filename'])
+  return (image_features,
+      label,
+      features['image/class/text'],
+      features['image/filename'])
 
 def batch_inputs(dataset, batch_size, train, num_preprocess_threads=None):
-    """ Contruct batches of training or evaluation examples from the video 
-        dataset.
+  """ Contruct batches of training or evaluation examples from the video 
+    dataset.
 
-    Args:
-        dataset: instance of Dataset class specifying the dataset. See 
-            dataset.py for details.
-        batch_size: integer
-        train: boolean, shuffle the dataset or not
-        num_preprocess_threads: integer, total number of preprocessing threads
+  Args:
+    dataset: instance of Dataset class specifying the dataset. See 
+      dataset.py for details.
+    batch_size: integer
+    train: boolean, shuffle the dataset or not
+    num_preprocess_threads: integer, total number of preprocessing threads
 
-    Returns:
-        videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
-            video, each video is a dictionary containing strings providing 
-            JPEG encoding of all the images of a video clip
-        labels: 1-D integer Tensor of [batch_size].
-        filenames: an array contains all the filenames
+  Returns:
+    videos: 2-D string Tensor of [batch_size, sequence_length] a batch of 
+      video, each video is a dictionary containing strings providing 
+      JPEG encoding of all the images of a video clip
+    labels: 1-D integer Tensor of [batch_size].
+    filenames: an array contains all the filenames
 
-    Raises:
-        ValueError: if data is not found
-    """
-    with tf.name_scope('batch_processing'):
-        data_files = dataset.data_files()
-        if data_files is None:
-            raise ValueError('No data files found for this dataset')
+  Raises:
+    ValueError: if data is not found
+  """
+  with tf.name_scope('batch_processing'):
+    data_files = dataset.data_files()
+    if data_files is None:
+      raise ValueError('No data files found for this dataset')
 
-        # Create filename_queue
-        if train:
-            filename_queue = tf.train.string_input_producer(data_files,
-                                                        shuffle=True,
-                                                        capacity=16)
-        else:
-            filename_queue = tf.train.string_input_producer(data_files,
-                                                        shuffle=False,
-                                                        capacity=1)
-        if num_preprocess_threads is None:
-            num_preprocess_threads = FLAGS.num_preprocess_threads
+    # Create filename_queue
+    if train:
+      filename_queue = tf.train.string_input_producer(data_files,
+                            shuffle=True,
+                            capacity=16)
+    else:
+      filename_queue = tf.train.string_input_producer(data_files,
+                            shuffle=False,
+                            capacity=1)
+    if num_preprocess_threads is None:
+      num_preprocess_threads = FLAGS.num_preprocess_threads
 
-        if num_preprocess_threads % 4:
-            raise ValueError('Please make num_preprocess_threads a multiple '
-                        'of 4 (%d % 4 != 0).', num_preprocess_threads)
+    if num_preprocess_threads % 4:
+      raise ValueError('Please make num_preprocess_threads a multiple '
+            'of 4 (%d % 4 != 0).', num_preprocess_threads)
 
-        # Approximate number of examples per shard.
-        examples_per_shard = 65
-        min_queue_examples = examples_per_shard * FLAGS.input_queue_memory_factor
-        if train:
-            examples_queue = tf.RandomShuffleQueue(
-                capacity=min_queue_examples + 3 * batch_size,
-                min_after_dequeue=min_queue_examples,
-                dtypes=[tf.string])
-        else:
-            examples_queue = tf.FIFOQueue(
-                capacity=examples_per_shard + 3 * batch_size,
-                dtypes=[tf.string])
+    # Approximate number of examples per shard.
+    examples_per_shard = 65
+    min_queue_examples = examples_per_shard * FLAGS.input_queue_memory_factor
+    if train:
+      examples_queue = tf.RandomShuffleQueue(
+        capacity=min_queue_examples + 3 * batch_size,
+        min_after_dequeue=min_queue_examples,
+        dtypes=[tf.string])
+    else:
+      examples_queue = tf.FIFOQueue(
+        capacity=examples_per_shard + 3 * batch_size,
+        dtypes=[tf.string])
 
-        reader = dataset.reader()
-        _, example_serialized = reader.read(filename_queue)
+    reader = dataset.reader()
+    _, example_serialized = reader.read(filename_queue)
 
-        videos_and_labels_and_filenames = []
-        
-        # Parse a serialized Example proto to extract the video and metadata.
-        image_features, label_index, _, filename = parse_example_proto(
-            example_serialized)
-        video = video_preprocessing(image_features)
-        videos_and_labels_and_filenames.append([video,
-                                                label_index, 
-                                                filename])
+    videos_and_labels_and_filenames = []
+    
+    # Parse a serialized Example proto to extract the video and metadata.
+    image_features, label_index, _, filename = parse_example_proto(
+      example_serialized)
+    video = video_preprocessing(image_features)
+    videos_and_labels_and_filenames.append([video,
+                        label_index, 
+                        filename])
 
-        videos, label_index_batch, filename_batch = tf.train.batch_join(
-            videos_and_labels_and_filenames,
-            batch_size=batch_size,
-            capacity=2 * num_preprocess_threads * batch_size)
+    videos, label_index_batch, filename_batch = tf.train.batch_join(
+      videos_and_labels_and_filenames,
+      batch_size=batch_size,
+      capacity=2 * num_preprocess_threads * batch_size)
 
-        # convert the label to one hot vector
-        labels = tf.reshape(label_index_batch, [batch_size])
-        labels_one_hot = tf.one_hot(labels, dataset.num_classes(), 1, 0)
+    # convert the label to one hot vector
+    labels = tf.reshape(label_index_batch, [batch_size])
+    labels_one_hot = tf.one_hot(labels, dataset.num_classes(), 1, 0)
 
-        return (videos, 
-                labels_one_hot,
-                tf.reshape(filename_batch, [batch_size]))
+    return (videos, 
+        labels_one_hot,
+        tf.reshape(filename_batch, [batch_size]))
