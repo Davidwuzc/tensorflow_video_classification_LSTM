72fe680d603c95b45ae458e4d21cd68b77e3813b
Dongfeng <gudongfeng@outlook.com>
2016-11-15 14:17:38 -0500

    change the bilsmt model back

--- bilstm_model.py
+++ bilstm_model.py
@@ -30,44 +30,58 @@ class BiLSTM(object):
     def __init__(self, is_training, input_, config, is_video = False):
         self._input = input_
 
-        # Deifine the LSTM cell
-        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(config.hidden_size, 
-                                                 forget_bias = 0.0, 
-                                                 state_is_tuple = True)
+        # Define lstm cells with tensorflow
+        # Forward direction cell
+        lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(config.hidden_size,
+                                                    state_is_tuple = True)
         if is_training and config.keep_prob < 1:
-            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
-                lstm_cell, output_keep_prob = config.keep_prob)
-        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers,     
-                                           state_is_tuple = True)
+            lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(
+                lstm_fw_cell, output_keep_prob = config.keep_prob)
+        cell_fw = tf.nn.rnn_cell.MultiRNNCell(
+            [lstm_fw_cell] * config.num_layers, 
+            state_is_tuple = True)
+        # Backward direction cell
+        lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(config.hidden_size,
+                                                    state_is_tuple = True) 
+        if is_training and config.keep_prob < 1:
+            lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(
+                lstm_bw_cell, output_keep_prob = config.keep_prob)
+        cell_bw = tf.nn.rnn_cell.MultiRNNCell(
+            [lstm_fw_cell] * config.num_layers, 
+            state_is_tuple = True)
 
         inputs = self._input.input_data
         if is_training and config.keep_prob < 1:
             inputs = tf.nn.dropout(inputs, config.keep_prob)
 
-        self._outputs, state = tf.nn.rnn(cell, inputs, dtype = tf.float32)
+        self._outputs, _, _ = tf.nn.bidirectional_rnn(
+            cell_fw, cell_bw, inputs, dtype = tf.float32)
 
         softmax_w = tf.get_variable("softmax_w", 
-            [config.hidden_size, config.num_classes])
-        softmax_b = tf.get_variable("softmax_b", [config.num_classes])
-
-        if not is_video:
+            [2 * config.hidden_size, config.num_classes])
+        softmax_b = tf.get_variable("softmax_b", [config.num_classes])            
+        
+        if is_video:
+            # Linear activation, using rnn inner loop last output
+            #   logit shape: [batch_size, num_classes]
+            self._logits = tf.matmul(self._outputs[-1], softmax_w) + softmax_b
+            self._cost = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(_logits, self._input.targets))
+        else:
             # Tranformthe outputs
             #   num_steps * [batch_size, hidden_size] =>
             #   [batch_size * num_steps, hidden_size]
             self._outputs = tf.reshape(
-                tf.concat(1, self._outputs), [-1, config.hidden_size])
-        
-        if is_video:
-            # Linear activation, using rnn inner loop last output
-            self._logits = tf.matmul(self._outputs[-1], softmax_w) + softmax_b
-        else:
+                tf.concat(1, self._outputs), [-1, 2 * config.hidden_size])
+            # logit shape: [batch_size * num_steps, num_classes]
             self._logits = tf.matmul(self._outputs, softmax_w) + softmax_b
 
-        loss = tf.nn.seq2seq.sequence_loss_by_example(
-            [self._logits],
-            [self._input.targets],
-            [tf.ones([config.batch_size * config.num_steps], dtype = tf.float32)])
-        self._cost = tf.reduce_sum(loss) / config.batch_size
+            loss = tf.nn.seq2seq.sequence_loss_by_example(
+                [self._logits],
+                [self._input.targets],
+                [tf.ones([config.batch_size * config.num_steps], dtype = tf.float32)])
+
+            self._cost = tf.reduce_sum(loss) / config.batch_size
 
         if not is_training:
             return
